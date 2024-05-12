24ee1bd76909304a988853d7f6f959ca6d2d912e
mohd anas <localmail.office@gmail.com>
2024-05-12 20:45:38 +0530

    add - fontfaimily

---
+++ app/fonts.tsx
@@ -0,0 +1,11 @@
+import { Playfair, Raleway } from "next/font/google";
+
+export const raleway = Raleway({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const playfair = Playfair({
+  subsets: ["latin"],
+  display: "swap",
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { use } from "react";
+import { playfair } from "./fonts";
 
 export default function Home() {
   return (
-    <div className="flex gap-5 p-5 items-center border m-5 rounded-lg">
-      <div>hello world</div>
-      <Button onClick={() => alert("hellow")}>click here</Button>
+    <div>
+      <div>Hero section</div>
     </div>
   );
 }
